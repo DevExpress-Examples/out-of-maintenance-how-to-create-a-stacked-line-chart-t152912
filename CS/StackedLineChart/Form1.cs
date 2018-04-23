7bdd75a911d68319e9f06e441afa4b90f44dbcd9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/StackedLineChart/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+using DevExpress.Utils;
+
+namespace StackedLineChart {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a new chart.
+            ChartControl stackedLineChart = new ChartControl();
+
+            // Create stacked line series.
+            Series series1 = new Series("Series 1", ViewType.StackedLine);
+            Series series2 = new Series("Series 2", ViewType.StackedLine);
+            Series series3 = new Series("Series 3", ViewType.StackedLine);
+            Series series4 = new Series("Series 4", ViewType.StackedLine);
+
+            // Add points to them
+            series1.Points.Add(new SeriesPoint("A", 8100));
+            series1.Points.Add(new SeriesPoint("B", 4120));
+            series1.Points.Add(new SeriesPoint("C", -1300));
+            series1.Points.Add(new SeriesPoint("D", -1700));
+
+            series2.Points.Add(new SeriesPoint("A", 14500));
+            series2.Points.Add(new SeriesPoint("B", 18000));
+            series2.Points.Add(new SeriesPoint("C", 19500));
+            series2.Points.Add(new SeriesPoint("D", 21000));
+
+            series3.Points.Add(new SeriesPoint("A", -915));
+            series3.Points.Add(new SeriesPoint("B", -1348));
+            series3.Points.Add(new SeriesPoint("C", -2900));
+            series3.Points.Add(new SeriesPoint("D", -3423));
+
+            series4.Points.Add(new SeriesPoint("A", -6395));
+            series4.Points.Add(new SeriesPoint("B", -6494));
+            series4.Points.Add(new SeriesPoint("C", -7819));
+            series4.Points.Add(new SeriesPoint("D", -9567));
+
+            // Add series to the chart.
+            stackedLineChart.Series.AddRange(new Series[] { series1, series2, series3, series4 });
+
+            // Show series labels.
+            series1.LabelsVisibility = DefaultBoolean.True;
+            series2.LabelsVisibility = DefaultBoolean.True;
+            series3.LabelsVisibility = DefaultBoolean.True;
+            series4.LabelsVisibility = DefaultBoolean.True;
+
+            // Customize crosshair cursor options to compare series.   
+            stackedLineChart.CrosshairOptions.ShowArgumentLabels = true;
+            stackedLineChart.CrosshairOptions.ShowValueLabels = true;
+            stackedLineChart.CrosshairOptions.ShowValueLine = true;
+            stackedLineChart.CrosshairOptions.SnapMode = CrosshairSnapMode.NearestValue;
+
+            // Add a title to the chart (if necessary).
+            stackedLineChart.Titles.Add(new ChartTitle());
+            stackedLineChart.Titles[0].Text = "A Stacked Line Chart";
+
+            // Add the chart to the form.
+            stackedLineChart.Dock = DockStyle.Fill;
+            this.Controls.Add(stackedLineChart);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to create a Stacked Line chart
+
+
+<p>The following example demonstrates how to create a <a href="https://documentation.devexpress.com/WindowsForms/clsDevExpressXtraChartsChartControltopic.aspx">DevExpress.XtraCharts.ChartControl</a> with series of the <a href="https://documentation.devexpress.com/#CoreLibraries/clsDevExpressXtraChartsStackedLineSeriesViewtopic">StackedLineSeriesView</a>  type, and add this chart to a form at runtime. Before proceeding with this example, first create a Windows Forms Application in Visual Studio, and include all <a href="https://documentation.devexpress.com/WindowsForms/CustomDocument3160.aspx">necessary assemblies</a> to the References list of your project.</p>
+Then, add the following code to the <strong>Form.Load</strong> event handler.
+
+<br/>
+
+
